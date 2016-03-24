91f40c14624de6f29fd1126c112d7019b78d7d89
tab-liu <dearhange@126.com>
2016-03-24 20:27:47 +0800

    globalmem with mutiple

---
+++ globalmem/.globalmem.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/hange/code/book_examples/globalmem/globalmem.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hange/code/book_examples/globalmem/globalmem.ko /home/hange/code/book_examples/globalmem/globalmem.o /home/hange/code/book_examples/globalmem/globalmem.mod.o
---
+++ globalmem/.tmp_versions/globalmem.mod
@@ -0,0 +1,2 @@
+/home/hange/code/book_examples/globalmem/globalmem.ko
+/home/hange/code/book_examples/globalmem/globalmem.o
